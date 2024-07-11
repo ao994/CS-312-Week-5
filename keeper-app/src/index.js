6ea36cd7e8485117cf81c37e7c038d5a18fd87c8
Alyssa Ortiz <ao994@nau.edu>
2024-07-10 19:37:16 -0700

    added all files
    did not include node modules

---
+++ keeper-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+//changed; ReactDOM.render is depreciated
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
